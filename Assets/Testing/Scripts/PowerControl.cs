579d4180cef841dc97acef3cfb18cf59ef0fc093
CodeLeopard <arvid.kater@gmail.com>
2020-05-11 22:33:38 +0200

    More space-to-tab conversion

--- Assets/Testing/Scripts/PowerControl.cs
+++ Assets/Testing/Scripts/PowerControl.cs
@@ -8,66 +8,66 @@ using UnityEngine;
 public class PowerControl : MonoBehaviour
 {
 
-    [SerializeField] public int capacity = 100;
+	[SerializeField] public int capacity = 100;
 
-    public int currentCharge;
-    public bool dead;
-    public bool maxcharged;
+	public int currentCharge;
+	public bool dead;
+	public bool maxcharged;
 
-    private void Awake()
-    {
-        currentCharge = capacity;
-        dead = false;
-        maxcharged = true;
-    }
+	private void Awake()
+	{
+		currentCharge = capacity;
+		dead = false;
+		maxcharged = true;
+	}
 
-    private void FixedUpdate()
-    {
-        if (currentCharge < 0)
-        {
-            dead = true;
-            currentCharge = 0;
-        }
+	private void FixedUpdate()
+	{
+		if (currentCharge < 0)
+		{
+			dead = true;
+			currentCharge = 0;
+		}
 
-        if (currentCharge < capacity && maxcharged)
-        {
-            maxcharged = false;
-        }
-    }
+		if (currentCharge < capacity && maxcharged)
+		{
+			maxcharged = false;
+		}
+	}
 
-    private void Update()
-    {
+	private void Update()
+	{
 
-    }
+	}
 
-    //To drain power call PowerControl.Drain(amount to draw)
-    public void Drain(int amount)
-    {
-        if (!dead)
-        {
-            currentCharge -= amount;
+	//To drain power call PowerControl.Drain(amount to draw)
+	public void Drain(int amount)
+	{
+		if (!dead)
+		{
+			currentCharge -= amount;
 
-            if (currentCharge <= 0)
-            {
-                currentCharge = 0;
-                dead = true;
-            }
-        }
-    }
+			if (currentCharge <= 0)
+			{
+				currentCharge = 0;
+				dead = true;
+			}
+		}
+	}
 
-    //To drain power call PowerControl.Charge(amount to add)
-    public void Charge(int amount)
-    {
-        if (!maxcharged)
-        {
-            currentCharge += amount;
+	//To drain power call PowerControl.Charge(amount to add)
+	public void Charge(int amount)
+	{
+		if (!maxcharged)
+		{
+			currentCharge += amount;
 
-            if (currentCharge >= capacity)
-            {
-                currentCharge = capacity;
-                maxcharged = true;
-            }
-        }
-    }
+			if (currentCharge >= capacity)
+			{
+				currentCharge = capacity;
+				maxcharged = true;
+			}
+		}
+	}
 }
 
