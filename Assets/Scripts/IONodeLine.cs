ea9fda95d2a46301d78dece24cf4bb028f414474
Math Campbell-Sturgess <math.campbell@gmail.com>
2020-05-14 01:22:21 +0100

    Nodes are fully working.
    Fixed bugs meaning if you try to re-draw an existing node connection it will delete itself, and if you connect an output to an input that has already got a node, it will overwrite it.

---
+++ Assets/Scripts/IONodeLine.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IONodeLine : MonoBehaviour
+{
+    public OnOffInput ConnectedInput;
+    public OnOffOutput ConnectedOutput;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool IsConnected()
+    {
+        if (ConnectedInput && ConnectedOutput)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+}
