eb3c6d5ec0ae72cbf901b32c584b8d511a7523bb
CodeLeopard <arvid.kater@gmail.com>
2020-07-01 03:39:39 +0200

    Add diffirential, GearBox, Motor

---
+++ Assets/Testing/MechanicalPower/GearBox.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Testing.MechanicalPower
+{
+	/// <summary>
+	/// A <see cref="Component"/> that links two <see cref="Network"/>s with a selectable ratio between them
+	/// </summary>
+	public class GearBox : EdgeComponent
+	{
+		protected Network network2;
+
+		int selectedRatioIndex;
+		List<float> ratios;
+
+
+
+
+		public override List<Network> CurrentleConnectedNetworks(SuperNetwork activeNetwork)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void ShaftUpdate(SuperNetwork activeNetwork)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
