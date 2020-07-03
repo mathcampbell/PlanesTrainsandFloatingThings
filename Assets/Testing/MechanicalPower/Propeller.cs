caab0750680da50b7cac390d5173343492e245be
CodeLeopard <arvid.kater@gmail.com>
2020-07-03 18:06:59 +0200

    Add propeller and Wheel ShaftComponents

---
+++ Assets/Testing/MechanicalPower/Propeller.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Testing.MechanicalPower
+{
+	public class Propeller : Component
+	{
+		/// <summary>
+		/// The radius of the propeller.
+		/// </summary>
+		private float radius;
+
+		/// <summary>
+		/// The amount of blades on the propeller
+		/// </summary>
+		private int bladeCount;
+
+		//TODO: Other performance parameters of the propeller.
+		//TODO? ideally water and air propellers just vary in these parameters and we don't have seperate classes. Is this feasable?
+
+
+
+
+
+		/// <inheritdoc />
+		public override void ShaftUpdate()
+		{
+			var cu = network.superNetwork.CU;
+
+			float rpm = cu.RPM;
+
+			float forwardAirSpeed = 1; //TODO get airspeed at the position of the propeller (because vehicle/subgrid rotation etc).
+			float pressure = 1;       //TODO compute from altitude (vehicle's altitude will be close enough for the math).
+
+			//TODO: Water? -> In water more frition/pressure etc.
+
+			float torque = forwardAirSpeed * pressure * rpm * radius * bladeCount; //TODO: Math
+
+			float forwardForce = pressure * rpm * radius * bladeCount; //TODO: Math
+
+			cu.AddTorque(torque);
+
+			//TODO Add force to vehicle (subGrid)
+			//rigidBody.AddForce(thisBlock.Forward * forwardForce); // or something like that <-
+		}
+	}
+}
---
+++ Assets/Testing/MechanicalPower/Wheel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+namespace Assets.Testing.MechanicalPower
+{
+public class Wheel : Component
+{
+	private WheelCollider collider;
+
+	/// <summary>
+	/// Variable that defines how much torque is generated from the RPM diffirence between the wheel and the shaft.
+	/// </summary>
+	private float magicVariable = 1;
+
+	/// <inheritdoc />
+	public override void ShaftUpdate()
+	{
+		var active = network.superNetwork.CU; // Shortcut.
+
+		float rpmDelta = collider.rpm - active.RPM;
+
+		float torque = rpmDelta * magicVariable;
+		// Options to replace magicVariable:
+		// PID
+		// More math? (using wheel inertia)
+
+
+		//TODO: Directions of the torque could be wrong.
+		active.AddTorque(torque);
+		collider.motorTorque = torque;
+	}
+}
+}
