03f40e4fd4b3dbca70f56f8011f66ed41583df24
Math Campbell-Sturgess <math.campbell@gmail.com>
2020-07-03 14:24:40 +0100

    meta stuff I think

---
+++ Assets/Testing/MechanicalPower/CombustionEngine.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Testing.MechanicalPower
+{
+	public class CombustionEngine : Component
+	{
+		/// <summary>
+		/// Throttle, [-1..1]
+		/// </summary>
+		public float throttle;
+
+		/// <summary>
+		/// The torque/RPM curve of the engine. Must be manually set in Editor and saved to Engine prefab.
+		/// </summary>
+		public AnimationCurve torqueCurve; // This would normally run from 0 to 7200 RPM for most gasoline engines; torque produced will change for each engine.
+
+
+		/// <summary>
+		/// The RPM at which torque drops to zero because of the redline.
+		/// </summary>
+		public float maxRPM; // 7200
+
+
+		/// <summary>
+		/// The maximum output torque. (at 100% <see cref="throttle"/>
+		/// </summary>
+		public float torqueOutput;
+
+
+		public override void ShaftUpdate()
+		{
+			var cu = network.superNetwork.CU;
+
+			cu.AddFriction(cu.RPM * frictionLoss);
+			
+			float absRPM = Math.Abs(cu.RPM);
+
+			float torque = 0;
+
+			if(absRPM < maxRPM)
+			{
+				torque = throttle * torqueCurve.Evaluate(absRPM);
+			}
+            else
+			{
+				
+				// else torque = 0
+			}
+
+			cu.AddTorque(torque);
+
+			// https://www.quora.com/What-is-the-formula-to-calculate-the-power-consumed-by-an-electric-motor
+			float powerDrawWatt = 2 * Mathf.PI * absRPM * torque / 60;
+		}
+	}
+}
