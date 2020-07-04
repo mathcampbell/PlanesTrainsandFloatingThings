c475bfebff013459fa1fa38bbd7c37f87b8eae67
Math Campbell-Sturgess <math.campbell@gmail.com>
2020-07-04 22:37:34 +0100

    meta stuff

---
+++ Assets/Scripts/PressureLookup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/PressureLookupScriptableObject", order = 1)]
+public class PressureLookupScriptableObject : ScriptableObject
+{
+    public  AnimationCurve airPressure;
+	public  AnimationCurve waterPressure;
+
+	public  float AirPressure(float altitude)
+	{
+		return airPressure.Evaluate(altitude);
+	}
+
+    public  float WaterPressure(float depth)
+    {
+		return waterPressure.Evaluate(depth);
+    }
+
+}
