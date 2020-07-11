71d3f421da7de2eccca07047842f503c7a7084cd
CodeLeopard <arvid.kater@gmail.com>
2020-07-12 01:01:40 +0200

    Add operators to ConversionInfo

--- Assets/Testing/MechanicalPower/ConversionInfo.cs
+++ Assets/Testing/MechanicalPower/ConversionInfo.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using JetBrains.Annotations;
+
 namespace Assets.Testing.MechanicalPower
 {
 	/// <summary>
@@ -39,5 +41,36 @@ namespace Assets.Testing.MechanicalPower
 			Torque = this.Torque;
 			Orientation = this.Orientation;
 		}
+
+		#region Operators
+
+		public static ConversionInfo operator -(ConversionInfo a)
+		{
+			(float rpm, float torque, float orientation) = a;
+			return new ConversionInfo(1/rpm, 1/torque, 1/orientation);
+		}
+
+		public static ConversionInfo operator *(ConversionInfo left, ConversionInfo right)
+		{
+			(float r1, float t1, float o1) = left;
+			(float r2, float t2, float o2) = right;
+			return new ConversionInfo(rPM: r1 * r2, torque: t1 * t2, orientation: o1 * o2);
+		}
+
+		public static ConversionInfo operator *(ConversionInfo left, float right)
+		{
+			(float rpm, float torque, float orientation) = left;
+			return new ConversionInfo(rPM: rpm * right, torque: torque * right, orientation: orientation * right);
+		}
+
+		public static ConversionInfo operator /(ConversionInfo left, float right)
+		{
+			(float rpm, float torque, float orientation) = left;
+			return new ConversionInfo(rPM: rpm / right, torque: torque / right, orientation: orientation / right);
+		}
+
+
+
+		#endregion Operators
 	}
 }
