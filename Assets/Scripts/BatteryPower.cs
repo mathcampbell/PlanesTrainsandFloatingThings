96de57e711bf8365bf5f7fb72e1cef3bf5eb888c
Math Campbell-Sturgess <math.campbell@gmail.com>
2020-07-22 18:15:15 +0100

    removed old code no longer needed.

---
+++ Assets/Scripts/BatteryPower.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryPower : PowerStorage
+{
+
+    public new readonly float capacity = 75000;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Capacity, is in watts - 1W = 1J/per second. This battery will power 20W of power for about one hour. 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Camera.main.transform.position, -Vector3.up);
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
--- Assets/Scripts/GenericConsumer.cs
+++ Assets/Scripts/GenericConsumer.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BatteryPower : PowerStorage
+public class GenericConsumer : PowerConsumer
 {
 
-    public new readonly float capacity = 75000;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        // Capacity, is in watts - 1W = 1J/per second. This battery will power 20W of power for about one hour.
+         
+
     }
 
     // Update is called once per frame
@@ -21,6 +22,21 @@ public class BatteryPower : PowerStorage
 
     void FixedUpdate()
     {
-        
+
     }
+
+
+    public override float  PotentialConsumption()
+    {
+        return requested;
+    }
+
+    public override void  Consume(float fractionOfPotential)
+    {
+        recieved = fractionOfPotential;
+    }
+    
+
+
+
 }
