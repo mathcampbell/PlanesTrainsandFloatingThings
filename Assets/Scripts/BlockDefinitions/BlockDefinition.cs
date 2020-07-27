1d6c6b6bc7f0ebd9e02e964b15eed9a3f0290b0e
CodeLeopard <arvid.kater@gmail.com>
2020-07-27 17:15:54 +0200

    Move BlockSides enum to HelperTypes

--- Assets/Scripts/BlockDefinitions/BlockDefinition.cs
+++ Assets/Scripts/BlockDefinitions/BlockDefinition.cs
@@ -14,22 +14,7 @@ using UnityEngine.PlayerLoop;
 
 namespace BlockDefinitions
 {
-	/// <summary>
-	/// An Flags Enum to define sides of a single block.
-	/// </summary>
-	[Flags]
-	public enum BlockSides : UInt16
-	{
-		// We need to set the values explicitly, because it defaults to just counting upwards.
-		  None   = 0b_000000
-		, Top    = 0b_000001
-		, Bottom = 0b_000010
-		, Left   = 0b_000100
-		, Right  = 0b_001000
-		, Front  = 0b_010000
-		, Rear   = 0b_100000
-		, All    = Top | Bottom | Left | Right | Front | Rear // 0b_111111
-	}
+	
 
 
 	/// <summary>
