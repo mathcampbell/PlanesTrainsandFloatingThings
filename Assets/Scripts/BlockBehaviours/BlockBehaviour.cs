5c0d46ba8b68b99d0c157c3f17b1d1d18626cb19
CodeLeopard <arvid.kater@gmail.com>
2020-08-01 00:03:42 +0200

    Add BlockBehaviour

---
+++ Assets/Scripts/BlockBehaviours/BlockBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using BlockDefinitions;
+
+using UnityEngine;
+
+using Vehicle.Blocks;
+
+namespace BlockBehaviours
+{
+	public class BlockBehaviour : MonoBehaviour
+	{
+		public Block blockDesign;
+
+		[NonSerialized]
+		public BlockDefinition blockDefinition;
+
+		public BlockID blockID;
+
+		#region Visual Representation
+		// Todo: Simple blocks should be merged, both for collision and rendering.
+
+		[NonSerialized]
+		public MeshFilter meshFilter;
+
+		[NonSerialized]
+		public Renderer renderer;
+
+
+		void Awake()
+		{
+			blockDefinition = BlockDefinition.Definitions[blockID];
+		}
+
+
+		#endregion Visual Representation
+	}
+}
