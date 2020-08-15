84c08a149ae20bcd385cfd88c6c1fc84f831fe30
mathcampbell <math.campbell@gmail.com>
2020-08-15 17:07:33 +0100

    Unity 2020.1.1 Update
    Updated project version to Unity 2020.1.1f1

--- Assets/Scripts/Vehicle/Blocks/Block.cs
+++ Assets/Scripts/Vehicle/Blocks/Block.cs
@@ -60,13 +60,14 @@ namespace Vehicle.Blocks
 
 		[NonSerialized]
 		public float sidelength; // Todo: @Math should this be moved to BlockDefinition? What is it used for? (since not all blocks will be square)
-
+								 //		@Leopard: this was a poor attempt at a simplified centre of gravity & mass solution that isn't quite implemented yet; since we can't get the CoG using Unity's physics cos we aren't making every block a Rigidbody
+								 //				we will need to manually calculate it, which therefore required knowing how big each block it, it's relative position in the vehicle and it's mass, to accurate get the CoG and CoM.
 		[NonSerialized]
-		public float volume; // Todo: @Math idk if buoyancy will be as simple as just this. If it's to be used: should be moved to Definition.
+		public float volume; // Todo: @Math idk if buoyancy will be as simple as just this. If it's to be used: should be moved to Definition. @Leopard - yeah this was before we worked out about enclosed volumes etc;
+							//			we might still want to use it though - if we have the mass & volume of each block, we can get density, which is needed for the bouyancy. We'd just have to remember to add on the enclosed volumes too (at their relative mass/density, which might change based on water or gas content).
+        #region Rendering
 
-		#region Rendering
-
-		[NonSerialized]
+        [NonSerialized]
 		protected MeshFilter meshFilter;
 
 		[NonSerialized]
@@ -76,7 +77,7 @@ namespace Vehicle.Blocks
 		public Material[] matArray;
 
 		[NonSerialized]
-		public Animator BlockAnim; // todo: @Math is this VehicleEditor only ?
+		public Animator BlockAnim; // todo: @Math is this VehicleEditor only ? @Leopard yes at the moment - but we might want to keep it around becasue we can also use it for damage/fire animations.
 
 		#endregion RuntimeState, Todo: (re)move
 
