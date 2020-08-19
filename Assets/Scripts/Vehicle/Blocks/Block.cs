c43ff75dca2785719ec7518e2c4157b679da5953
CodeLeopard <arvid.kater@gmail.com>
2020-08-19 17:25:10 +0200

    Serialization for Block

--- Assets/Scripts/Vehicle/Blocks/Block.cs
+++ Assets/Scripts/Vehicle/Blocks/Block.cs
@@ -1,7 +1,9 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-
+using System.IO;
+using System.Runtime.Serialization;
+using System.Xml;
 
 using BlockDefinitions;
 
@@ -13,15 +15,16 @@ namespace Vehicle.Blocks
 	/// The block class, and derived classes will hold the data related to a vehicle's design,
 	/// so it's position (,etc.) and the properties that may have been set in the VehicleEditor.
 	/// </summary>
-	public class Block : MonoBehaviour // Todo: Should not be MonoBehaviour. But there should be a representaiton for the block with monobehaviour so ...
+	[DataContract]
+	public class Block : IDeserializationCallback
 	{
 		#region Definition
 		// Shortcuts to the Definition of this block.
 
 		[NonSerialized]
-		protected BlockDefinition myBlockDefinition;
+		internal BlockDefinition myBlockDefinition;
 
-		[SerializeField]
+		[DataMember]
 		public readonly BlockID blockID;
 
 		public bool IsSingleCubeBlock => myBlockDefinition.IsSingleCubeBlock;
@@ -41,13 +44,45 @@ namespace Vehicle.Blocks
 		#region Design
 		// How this block relates to the vehicle.
 
+		[DataMember]
 		public Vector3Int position;
 
+		[DataMember]
 		public Orientation orientation;
 
 
 		#endregion
 
-		
+		/// <inheritdoc />
+		public void OnDeserialization(object sender)
+		{
+			myBlockDefinition = BlockDefinition.Definitions[blockID];
+		}
+
+
+		#region Static
+		#region Serialization
+
+		private static readonly DataContractSerializer Serializer = new DataContractSerializer(typeof(Block));
+		private static readonly XmlDictionary SerializerDictionary = new XmlDictionary();
+
+		public static void WriteToStreamBinary(Stream s, Block data)
+		{
+			using (var writer = XmlDictionaryWriter.CreateBinaryWriter(s))
+			{
+				Serializer.WriteObject(writer, data);
+			}
+		}
+
+		public static Block ReadFromStreamBinary(Stream s)
+		{
+			using (var reader = XmlDictionaryReader.CreateBinaryReader(s, SerializerDictionary, XmlDictionaryReaderQuotas.Max))
+			{
+				return (Block)Serializer.ReadObject(reader);
+			}
+		}
+
+		#endregion Serialization
+		#endregion Static
 	}
 }
