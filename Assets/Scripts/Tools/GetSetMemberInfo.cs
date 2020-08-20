196cb68d896950119299ecb2f62fbb12650997b5
CodeLeopard <arvid.kater@gmail.com>
2020-08-20 03:54:33 +0200

    GetSetmemberInfo actually works now

--- Assets/Scripts/Tools/GetSetMemberInfo.cs
+++ Assets/Scripts/Tools/GetSetMemberInfo.cs
@@ -10,6 +10,8 @@ namespace Tools
 	{
 		private readonly FieldInfo field;
 		private readonly PropertyInfo property;
+		private readonly MethodInfo getter;
+		private readonly MethodInfo setter;
 
 		private readonly bool isField;
 
@@ -45,6 +47,11 @@ namespace Tools
 			else
 			{
 				MemberDataType = property.PropertyType;
+				getter = property.GetGetMethod
+					         (true)
+				      ?? property.DeclaringType.GetProperty(property.Name).GetGetMethod(true);
+				setter = property.GetSetMethod(true)
+				      ?? property.DeclaringType.GetProperty(property.Name).GetSetMethod(true);
 			}
 		}
 
@@ -61,7 +68,7 @@ namespace Tools
 			}
 			else
 			{
-				property.SetValue(instance, value);
+				setter.Invoke(instance, new[] { value });
 			}
 		}
 
@@ -72,7 +79,7 @@ namespace Tools
 		/// <param name="instance">the instance to retrieve from</param>
 		/// <returns>The value of the member on <paramref name="instance"/></returns>
 		/// <exception cref="InvalidCastException">if data was null or <typeparamref name="TData"/>was wrong</exception>
-		public TData GetValue<TData>(object instance)
+		public TData GetValueStrict<TData>(object instance)
 		{
 			if (isField)
 			{
@@ -80,7 +87,7 @@ namespace Tools
 			}
 			else
 			{
-				return (TData) property.GetValue(instance);
+				return (TData) getter.Invoke(instance, null);
 			}
 		}
 
@@ -90,7 +97,7 @@ namespace Tools
 		/// <typeparam name="TData">The type of the data</typeparam>
 		/// <param name="instance">the instance to retrieve from</param>
 		/// <returns>The value of the member on <paramref name="instance"/>, or null</returns>
-		public TData GetValueClass<TData>(object instance) where TData : class
+		public TData GetValue<TData>(object instance) where TData : class
 		{
 			if (isField)
 			{
@@ -98,7 +105,7 @@ namespace Tools
 			}
 			else
 			{
-				return property.GetValue(instance) as TData;
+				return getter.Invoke(instance, null) as TData;
 			}
 		}
 
