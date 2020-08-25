a4cad1a7a3cb970685bbf8ee2652dbf2d1cd2e01
CodeLeopard <arvid.kater@gmail.com>
2020-08-25 19:12:30 +0200

    Add Readonly Property for Inspector Properties

---
+++ Assets/Scripts/Tools/InspectorReadonlyAttribute.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Tools
+{
+	/// <summary>
+	/// This property marks the property as read-only for the Inspector panel in the Unity Editor
+	/// </summary>
+	public class InspectorReadonlyAttribute : PropertyAttribute
+	{
+	}
+}
