06435604bf02ce658bba11377570069949e7bd27
CodeLeopard <arvid.kater@gmail.com>
2020-10-12 17:22:21 +0200

    Start on testing UI for WorldSaveState

---
+++ Assets/Testing/Serialization/Scripts/SaveLoadTestBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using Serialization;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Testing.Serialization
+{
+	public class SaveLoadTestBehaviour : MonoBehaviour
+	{
+		public Button saveButton;
+		public Button loadButton;
+
+		public string fileName = "save";
+
+		public bool binary = false;
+
+
+		void Start()
+		{
+			saveButton.onClick.RemoveListener(OnSaveButton);
+			loadButton.onClick.RemoveListener(OnLoadButton);
+
+			saveButton.onClick.AddListener(OnSaveButton);
+			loadButton.onClick.AddListener(OnLoadButton);
+		}
+
+
+		void OnSaveButton()
+		{
+			WorldSaveState.Save(fileName, binary);
+		}
+
+		void OnLoadButton()
+		{
+			WorldSaveState.Load(fileName);
+		}
+	}
+}
