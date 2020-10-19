bbc9d2b34258d45ddb4cb111b5930f32f400df67
CodeLeopard <arvid.kater@gmail.com>
2020-10-19 19:24:57 +0200

    Start on BlockFaces (2b used 4 Raycast in Editor)

---
+++ Assets/Scripts/BlockDefinitions/Types/BlockFace.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+using UnityEngine;
+
+namespace BlockDefinitions.Types
+{
+	[DataContract]
+	public struct BlockFace
+	{
+		/// <summary>
+		/// The gridPosition where a face will be defined.
+		/// </summary>
+		public Vector3Int GridPosition;
+
+		/// <summary>
+		/// The side(s) of that position where a face will be.
+		/// </summary>
+		public BlockSides side;
+
+		/// <summary>
+		/// The shape of the face.
+		/// </summary>
+		public BlockFaceShape shape;
+
+	}
+}
---
+++ Assets/Scripts/BlockDefinitions/Types/BlockFaceShape.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace BlockDefinitions.Types
+{
+	[DataContract]
+	public enum BlockFaceShape : UInt16
+	{
+		None     = 0,
+		Square   = 1,
+		Triangle = 2,
+
+	}
+}
