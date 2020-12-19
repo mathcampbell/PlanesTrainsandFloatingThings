260f102305056c10c27b5bf5607785a9b4ebbba1
CodeLeopard <arvid.kater@gmail.com>
2020-12-19 21:49:39 +0100

    Add documentation to GameObjectExt

--- Assets/Scripts/DataTypes/Extensions/GameObjectExt.cs
+++ Assets/Scripts/DataTypes/Extensions/GameObjectExt.cs
@@ -4,13 +4,45 @@ using UnityEngine;
 
 namespace DataTypes.Extensions
 {
-	public static class GameObjectExt {
+	/// <summary>
+	/// Extensions for GameObjects
+	/// </summary>
+	public static class GameObjectExt
+	{
 
+		/// <summary>
+		/// Does the given <see cref="GameObject"/> have component <typeparamref name="T"/>?
+		/// </summary>
+		/// <typeparam name="T">The component to look for</typeparam>
+		/// <param name="obj">The object to inspect</param>
+		/// <returns></returns>
 		public static bool HasComponent <T>(this GameObject obj) where T : Component
 		{
 			return obj.GetComponent<T>() != null;
 		}
 
+		/// <summary>
+		/// Get an existing component, or add it if it doesn't exist yet.
+		/// </summary>
+		/// <typeparam name="T">The component to look for or add</typeparam>
+		/// <param name="obj">The object to inspect</param>
+		/// <returns></returns>
+		public static bool GetOrAddComponent<T>(this GameObject obj) where T : Component
+		{
+			var component = obj.GetComponent<T>();
+			if (null != component)
+				return component;
+			else
+				return obj.AddComponent<T>();
+		}
+
+		/// <summary>
+		/// Get components in this object and it's children, if they have <paramref name="tag"/>
+		/// </summary>
+		/// <typeparam name="T">The component type to retrieve</typeparam>
+		/// <param name="gameObject">The object to inspect</param>
+		/// <param name="tag">The tag to check for</param>
+		/// <returns></returns>
 		public static List<T> GetComponentsInChildrenWithTag<T>(this GameObject gameObject, string tag)
 			where T : Component
 		{
