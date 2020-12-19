b257211cc44114070816a9bc463116f7e7572e62
CodeLeopard <arvid.kater@gmail.com>
2020-12-19 18:38:36 +0100

    Add ActiveBlock

---
+++ Assets/Scripts/Vehicle/Blocks/ActiveBlock.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace Vehicle.Blocks
+{
+	/// <summary>
+	/// The derived ActiveBlock classes will hold the data related to a vehicle's design and simulation,
+	/// so it's position (,etc.) and the properties that may have been set in the VehicleEditor and the simulation state.
+	/// </summary>
+	[DataContract]
+	public abstract class ActiveBlock : Block
+	{
+
+	}
+}
