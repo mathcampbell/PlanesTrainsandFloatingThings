0b49ee51ac84681b23a93693f35fb9f7564c371c
CodeLeopard <arvid.kater@gmail.com>
2020-12-19 21:47:43 +0100

    Move some more files

--- Assets/Scripts/DataTypes/Extensions/GameObjectExt.cs
+++ Assets/Scripts/DataTypes/Extensions/GameObjectExt.cs
@@ -3,35 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public static class ExtensionsHandy
-// The wrapper class name is actually irrelevant - it is not used at all.
-// Choose any convenient name for the wrapper class.
+	// The wrapper class name is actually irrelevant - it is not used at all.
+	// Choose any convenient name for the wrapper class.
 {
 
-	public static bool HasComponent <T>(this GameObject obj) where T : Component
+	public static bool HasComponent<T>(this GameObject obj) where T : Component
 	{
 		return obj.GetComponent<T>() != null;
 	}
 
 	public static bool IsNear(this float ff, float target)
 	{
-		float difference = ff-target;
+		float difference = ff - target;
 		difference = Mathf.Abs(difference);
-		if ( difference < 20f ) return true; // What? we arbitrarily decide < 20f is "near"?
+		if (difference < 20f) return true; // What? we arbitrarily decide < 20f is "near"?
 		else return false;
 	}
 
 	public static float Jiggle(this float ff)
 	{
-		return ff * UnityEngine.Random.Range(0.9f,1.1f);
+		return ff * UnityEngine.Random.Range(0.9f, 1.1f);
 	}
 
-	public static Color Colored( this float alpha, int r, int g, int b )
+	public static Color Colored(this float alpha, int r, int g, int b)
 	{
 		return new Color(
 			(float)r / 255f,
 			(float)g / 255f,
 			(float)b / 255f,
-			alpha );
+			alpha);
 	}
 
 	public static T[] GetComponentsInChildrenWithTag<T>(this GameObject gameObject, string tag)
