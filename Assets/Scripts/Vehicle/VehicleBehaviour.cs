0d8761eb7b4c706382b35f86f7d7023c29fc2a3a
CodeLeopard <arvid.kater@gmail.com>
2020-12-20 17:02:18 +0100

    Start on VehicleBehaviour

---
+++ Assets/Scripts/Vehicle/VehicleBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+using DataTypes;
+
+
+namespace Vehicle
+{
+	/// <summary>
+	/// The behaviour that manages the main vehicle's GameObject
+	/// </summary>
+	public class VehicleBehaviour : M0noBehaviour
+	{
+
+
+		private VehicleData vehicleData;
+
+
+
+	}
+}
