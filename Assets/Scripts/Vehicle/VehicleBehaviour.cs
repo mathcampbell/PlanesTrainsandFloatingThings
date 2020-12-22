2ae0e116b989317a254bae6871da643658c96933
CodeLeopard <arvid.kater@gmail.com>
2020-12-22 17:02:00 +0100

    Behaviour for trivial vehicle test

--- Assets/Scripts/Vehicle/VehicleBehaviour.cs
+++ Assets/Scripts/Vehicle/VehicleBehaviour.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 using UnityEngine;
 
 using DataTypes;
+
+using Tools;
+
+using Vehicle.Blocks;
 
 
 namespace Vehicle
@@ -11,6 +17,7 @@ namespace Vehicle
 	/// The behaviour that manages the main vehicle's GameObject through an instance of <see cref="VehicleData"/>.
 	/// </summary>
 	[DefaultExecutionOrder(defaultExecutionOrder)]
+	[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 	public class VehicleBehaviour : M0noBehaviour
 	{
 		public const int defaultExecutionOrder = 0;
@@ -19,12 +26,32 @@ namespace Vehicle
 		public VehicleData vehicleData;
 
 
+		public Material TempMaterial;
+
+		private MeshFilter meshFilter;
+		private new MeshRenderer renderer; // hide obsolete renderer in MonoBehaviour
+
+		private Mesh mainMesh;
+
+
 		/// <inheritdoc />
 		protected override void OnStart()
 		{
+			meshFilter = GetComponent<MeshFilter>();
+			renderer = GetComponent<MeshRenderer>();
+
 			if (null == vehicleData) throw new Exception($"{nameof(VehicleBehaviour)} with null {nameof(VehicleData)}");
 
 			vehicleData.Initialize();
+
+			mainMesh = MeshCombiner.CombineMeshes_Dumb(MeshCombinerData().ToList());
+			meshFilter.sharedMesh = mainMesh;
+
+			renderer.materials = new Material[mainMesh.subMeshCount];
+			for (int i = 0; i < renderer.materials.Length; i++)
+			{
+				renderer.materials[i] = TempMaterial;
+			}
 		}
 
 
@@ -39,6 +66,29 @@ namespace Vehicle
 			vehicleData.UnityUpdate();
 		}
 
+
+
+
+		private IEnumerable<MeshCombiner.DataItem> MeshCombinerData()
+		{
+			foreach (var block in vehicleData.blocks)
+			{
+				if (null != block.myBlockDefinition.Mesh)
+				{
+					var pos = (Vector3) block.position * Block.DesignToWorldScale;
+					var rot = Quaternion.identity; // todo: implement properly
+					var scl = Vector3.one;
+
+					var transform = Matrix4x4.Rotate(rot) * Matrix4x4.Scale(scl) * Matrix4x4.Translate(pos);
+
+					yield return new MeshCombiner.DataItem(block.myBlockDefinition.Mesh, transform);
+				}
+				else
+				{
+					// todo: implement properly (use BlockFaces)
+				}
+			}
+		}
 
 	}
 }
