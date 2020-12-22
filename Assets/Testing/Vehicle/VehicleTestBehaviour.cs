5cca79f9605db44b6cf58af30dcf6982ca6cc568
CodeLeopard <arvid.kater@gmail.com>
2020-12-22 15:48:50 +0100

    Add testing behaviour for vehicle

---
+++ Assets/Testing/Vehicle/VehicleTestBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using DataTypes;
+
+using UnityEngine;
+
+using Vehicle;
+
+namespace Testing.Vehicle
+{
+	public class VehicleTestBehaviour : M0noBehaviour
+	{
+		private GameObject vehicleObject;
+
+
+		/// <inheritdoc />
+		protected override void OnEnableAndAfterStart()
+		{
+			if (null != vehicleObject)
+			{
+				Destroy(vehicleObject);
+			}
+
+			vehicleObject = new GameObject("vehicle");
+
+			var vehicleBehaviour = vehicleObject.AddComponent<VehicleBehaviour>();
+
+			vehicleBehaviour.vehicleData = ZHardcodeVehicleDesigns.vehicle1;
+		}
+	}
+}
