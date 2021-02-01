9c6fe575ab816b08f1e092b8a0d488bc1e0701c2
CodeLeopard <arvid.kater@gmail.com>
2021-02-01 22:53:45 +0100

    Cleanup

--- Assets/Scripts/Vehicle/VehicleData.cs
+++ Assets/Scripts/Vehicle/VehicleData.cs
@@ -32,14 +32,6 @@ namespace Vehicle
 	[DataContract]
 	public class VehicleData
 	{
-		// todo: is the following still true?
-			// This class will be added to the root object of any vehicle added to a world scene.
-
-			// It will need to spawn a GridController to provide the grid of blocks that the root object has,
-			// then work out and declare the EnclosedSpaces and ExposedSpaces.
-			// These should probably be a separate class tht records the pressure, liquid content, gas content etc
-			// It will also need to manage the camera system for third-person mode if a player is controlling the vehicle.
-
 
 		#region Design
 
